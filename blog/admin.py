46f93f19ea7bf50418bd92e9fe64f26f689d4c60
Fabio Junior <fjrleao@gmail.com>
2020-05-11 15:47:43 -0300

    Adiçao de models e configuraçoes iniciais de admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+@admin.register(Sessao)
+class SessaoAdmin(admin.ModelAdmin):
+    list_display = ['nome']
+    search_fields = ['nome']
+    prepopulated_fields = {
+        'slug' : ('nome', )
+    }
+
+@admin.register(Postagem)
+class PostagemAdmin(admin.ModelAdmin):
+    list_display = ['titulo']
+    search_fields = ['titulo']
+    prepopulated_fields = {
+        'slug' : ('titulo', )
+    }
+
+@admin.register(Comentario)
+class ComentarioAdmin(admin.ModelAdmin):
+    list_display = ['nome', 'aprovado', 'data']
+    search_fields = ['data']
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Sessao(models.Model):
+    nome = models.CharField(max_length=125, null=False)
+    slug = models.SlugField()
+
+    def __str__(self):
+        return self.nome
+
+class Postagem(models.Model):
+    titulo =  models.CharField(max_length=125, null=False)
+    texto = models.TextField(null=False)
+    slug = models.SlugField()
+    data = models.DateField(auto_now=True)
+    postar = models.BooleanField(default=False)
+    curtidas = models.IntegerField()
+    sessao = models.ForeignKey(Sessao, on_delete=models.CASCADE)
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.titulo
+
+class Comentario(models.Model):
+    nome = models.CharField(max_length=125, null=False, editable=False)
+    texto = models.TextField(max_length=255, null=False, editable=False)
+    avaliacao = models.IntegerField(editable=False)
+    aprovado = models.BooleanField(default=False)
+    data = models.DateField(auto_now=True)
+    postagem = models.ForeignKey(Postagem, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nome
